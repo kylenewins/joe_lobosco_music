924c2862ffbb7a7ff1142464c9065f587554790d
Toad Sage <kylenewins@gmail.com>
2020-10-13 13:21:27 -0400

    docs tracklist update

--- src/app/animations.ts
+++ src/app/animations.ts
@@ -6,7 +6,7 @@ export const fadeIn = [
     trigger('fadeIn', [
         transition('void => *', [
           style({opacity: 0}),
-          animate(2000, style({opacity:1}))
+          animate(1000, style({opacity:1}))
       ])    
     ])
 ]
--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,9 +1,13 @@
 import { Component, OnInit } from '@angular/core';
+import {fadeIn} from "../animations"
 
 @Component({
   selector: 'app-contact',
   templateUrl: './contact.component.html',
-  styleUrls: ['./contact.component.css']
+  styleUrls: ['./contact.component.css'],
+  animations: [
+    fadeIn
+  ]
 })
 export class ContactComponent implements OnInit {
 
